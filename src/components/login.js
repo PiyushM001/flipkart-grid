ab56eff584e5fb21085db867f5d364e551a7bb19
piyush_mehta <pmimpiyush@gmail.com>
2024-12-31 19:12:30 +0530

    changed login

--- src/components/login.js
+++ src/components/login.js
@@ -18,7 +18,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("http://localhost:3001/login", {
+      const response = await fetch("https://flipkart-grid-backend-2.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
